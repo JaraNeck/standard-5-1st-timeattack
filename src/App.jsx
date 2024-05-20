0275b0806c3e2b39bb6399b5af01ef9a8dbca6e3
unknown <choipoe@gmail.com>
2024-05-20 15:52:14 +0900

    실패

--- src/App.jsx
+++ src/App.jsx
@@ -1,9 +1,31 @@
+import { useState } from "react";
+
 export default function App() {
+  const [add, setAdd] = useState();
+  const [minus, setMinus] = useState();
+
+  const onSubmit = (e) => {
+    e.peventDefault();
+    const setAdd = (add) => {
+      add = add + add;
+      return;
+      const setMinus = (minus) => {
+        minus = minus - minus;
+        return;
+      };
+    };
+  };
   return (
     <div>
       <h1>덧셈과 뺄셈이 가능한 앱 만들기</h1>
       <div>
-        <input /> 만큼을 <button>더할게요</button> <button>뺄게요</button>
+        <input onSubmit={onSubmit} /> 만큼을{" "}
+        <button type="number" setAdd={setAdd}>
+          더할게요
+        </button>{" "}
+        <button type="number" setMinus={setMinus}>
+          뺄게요
+        </button>
         <button>초기화</button>
       </div>
       <hr />
